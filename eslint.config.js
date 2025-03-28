3b567f86f7dd7022dc94e2ba7ba7686ef2c6f933
Emanuel0428 <cod2877@gmail.com>
2025-03-27 23:42:21 -0500

    test payu

--- eslint.config.js
+++ eslint.config.js
@@ -19,6 +19,10 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
